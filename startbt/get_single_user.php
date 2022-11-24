eaccde8c0a007b29bb423a66374f97f56dda8f03
Deborahjegab <99710480+Deborahjegab@users.noreply.github.com>
2022-11-24 14:07:02 +0200

    Delete startbt directory

--- startbt/get_single_user.php
+++
@@ -1,7 +0,0 @@
-<?php include ('config.php');
-$id =$_POST['id'];
-$sql ="SELECT * FROM staff_register WHERE id ='$id' ";
-$query =mysqli_query($conn,$sql);
-$customer =mysqli_fetch_assoc($query);
-echo json_encode($customer);
-?>  
